c6746c08d8940814bbe844f55ee4b33436c59b59
freddersly <scfred4@gmail.com>
2026-06-26 20:11:15 -0500

    Add global usings for RotationSolver namespaces

---
+++ GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using RotationSolver.Basic;
+global using RotationSolver.Basic.Actions;
+global using RotationSolver.Basic.Attributes;
+global using RotationSolver.Basic.Data;
+global using RotationSolver.Basic.Helpers;
+global using RotationSolver.Basic.Rotations;
+global using RotationSolver.Basic.Rotations.Basic;
